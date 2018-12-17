c9ac7295763bf209953c26c7088c7978eee60749
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-12-16 20:47:58 -0700

    Clean up win build script; reconfigure targets, tests and DLL naming

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,15 +2,20 @@ mkdir builddir
 cd builddir
 
 cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DUSE_RELATIVE_PATHS=TRUE ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -G "NMake Makefiles JOM" ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DwxBUILD_TESTS=CONSOLE_ONLY ^
+    -DwxBUILD_VENDOR="" ^
+    -DwxMSVC_VERSION="140" ^
     ..
 if errorlevel 1 exit /B 1
 
-nmake
+cmake --build .
 if errorlevel 1 exit /B 1
-:: No make check
-nmake install
+
+cmake --build . --target test
+if errorlevel 1 exit /B 1
+
+cmake --build . --target install
 if errorlevel 1 exit /B 1
